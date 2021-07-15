2f2d9c6b93f455b5574b817579b1302ade24da50
CitlallyS <87487114+CitlallyS@users.noreply.github.com>
2021-07-15 10:23:03 -0500

    Create hola.java

---
+++ hola.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class demo2 {
+public static void main(String[] ar) {
+Scanner teclado = new Scanner(System.in);
+int n1, n2, r;
+System.out.print("Ingrese primer valor:");
+n1 = teclado.nextInt();
+System.out.print("Ingrese segundo valor:");
+n2 = teclado.nextInt();
+r=n1*n2;
+System.out.print("La multiplicación es :"+r);
+
+}
+}
